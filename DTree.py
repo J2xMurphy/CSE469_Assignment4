aa205a7644f64d13c8ca2f391101d1adf10d9e3c
Joseph Murphy <murphy53@buffalo.edu>
2016-11-28 21:59:24 -0500

    Add files via upload

--- DTree.py
+++ DTree.py
@@ -1,11 +1,11 @@
 import sys 
 import csv
 from Node import  Node
-
+solcnt= 0
 global data_file, n_record, n_attr, input_data, visited
 
 # Function used for input data configuration
-def config(args = "golf.csv"):
+def config(args = "golf_processed.csv"):
     print(args)
     global data_file, n_record, n_attr, input_data, visited
     data_file = args
@@ -27,21 +27,17 @@ def config(args = "golf.csv"):
 
 # Main function
 def execute():
-    master = []
+    master = master_maker(input_data)
     global solcnt
     solcnt = 0
-    for i in range(len(input_data[0])):
-        tm = set()
-        for j in range(len(input_data)):
-            tm.add(input_data[j][i])
-        master.append(tm)
-    print(master)
+    #print(master)
     solcnt = len(master[len(master)-1])
-    print(solcnt)
+    #print(solcnt)
     root = Node()
-    baka = "root\n"+build_tree(input_data,master)
+    baka = "root\n"+str(build_tree(input_data,master,"",[]))
     print("CAKA WAKA SHAKA")
     print(baka)
+    write_string('output.txt',baka)
     #print(input_data)
     traverse_tree(root, 0)
 
@@ -58,28 +54,38 @@ def fullattr(indx,data):
     for i in range(len(data)):
         final.append(attrless(indx,data[i]))
     return final
-
-def build_tree(data,master):
-    print("Building Tree")
-    print(data)
-    print(master)
-    print("\n")
+    
+def master_maker(input_data):
+    master = []
+    solcnt = 0
+    for i in range(len(input_data[0])):
+        tm = set()
+        for j in range(len(input_data)):
+            tm.add(input_data[j][i])
+        master.append(tm)
+    return master
+    
+def build_tree(data,master,ts,off):
+    #print("Building Tree")
     baka=""
-    #for i in range(len(data[0])-1):
-    #    g=fullattr(i,data)
-    #    print(g)
-    #a=find_gini(1,1,data)
-    #b=find_gini(2,1,data)
-    #c=find_gini(3,1,data)
-    #print(a)
-    #print(b)
-    #print(c)
-    #print(master)
+    nts = ""
+    #print(len(ts))
+    for i in range(len(ts)):
+        #print(ts[i])
+        if ts[i]=='|':
+            nts+="|"
+        else:
+            nts+=" "
+    if len(data[0])<3:
+        c=mayday(data)
+        print(c)
+        for ele in c:
+            baka+=nts+"|->(A"+str(len(off))+":"+str(ele[0])+")->"+str(ele[1])+"\n"
+        return '!'+baka
     smallest=1.0
     newlists = []
     newlen=[]
     for i in range(len(master)-1):
-        print("I wanna kill myself with: "+str(master[i]))
         ginis = []
         refug = []
         lens=[]
@@ -91,7 +97,6 @@ def build_tree(data,master):
             refug.append(g[1])
             g=g[0]
             ginis.append(g)
-            print(g)
         gin = 0.0
         for ele in ginis:
             gin+=ele
@@ -100,63 +105,65 @@ def build_tree(data,master):
             newlists=refug[:]
             next = i
             newlen=lens[:]
-    #gin = 1-gin
-        print("The Gini is"+str(gin))
-    print("We're gonna split on"+str(master[next])+" With ")
-    printee(newlists)
-    printee(newlen)
+    #printee(newlists)
+    #print(off,next)
+    ww = 0
+    for ele in off:
+        if ww>=ele:
+            next+=1
+    off.append(next)
     for i in range(len(newlen)):
         if newlen[i]>1:
-            newmaster=[]
-            build_tree(newlists[i],newmaster)
-    #find_gini(1,2,data)
-    #find_gini(2,2,data)
-    #find_gini(3,2,data)
+            nl = fullattr(next,newlists[i])
+            newmaster=master_maker(nl)
+            caw = build_tree(nl,newmaster,ts+"|--------> ",off)
+            if '!' in caw:
+                baka+=nts+"|------>(A"+str(next+1)+":"+str(i+1)+")\n"
+                baka+=caw[1:]
+            else:
+                baka+=nts+"|------>(A"+str(next+1)+":"+str(i+1)+")\n"
+                baka+=ts+caw
+        else:
+            baka+=nts+"|->(A"+str(next+1)+":"+str(i+1)+")->"+str(newlists[i][0][len(newlists[i][0])-1])+"\n"
     return baka
-# Find gini index of given attribute data and corresponding Labels
-    # @param  att Attribute data
-    # @param  lab Label data
-    # @return gini index of an attribute
+    
+    
 def find_gini(att, lab, data, lst):
     countattr = 0.0
     countlab = 0.0
     cc = 0.0
+
     abs = set()
     refug = []
-    global solcnt
     thistoo = []
+    ofeach = []
+    for i in range(solcnt):
+        ofeach.append(0.0)
     for i in range(len(data)):
         if data[i][len(data[i])-1]==lab:
             countlab+=1.0
         if data[i][lst]==att:
             countattr+=1.0
+            thistoo.append(data[i][len(data[i])-1])
             abs.add(data[i][len(data[i])-1])
             refug.append(data[i])
     for i in range(len(refug)):
         if refug[i][len(refug[0])-1]==1:
             cc+=1.0
-    print(str(cc)+"/"+str(countattr)+"&"+str(countattr-cc)+"/"+str(countattr)+ " of " + str(len(data)))
-    #print("CC is :"+str(cc))
-    n0 = (cc/countattr)**2
-    n1 = ((countattr-cc)/countattr)**2
-    final = 1-(n0+n1)
-    print("gini is  :"+str(final))
-    #print()
-    final = final*(countattr/len(data))
-    #print()
-    #print()
-    #return (countattr,refug,len(abs))
-    return (final,refug,len(abs))
-        # **-------------------Fill in here------------------------**/      
-        # These steps might help you:
-        #  - Find number of different values in attribute, number of different labels
-        #  - For each value i, find number of occurrences and number of corresponding labels to calculate ginisplit
-        #  - gini = sum of all ginisplit
+    for i in range(len(thistoo)):
+        ofeach[thistoo[i]-1]+=1.0
+    #print("HHAHAH"+str(ofeach))
+    finali = 1.0
+    #print(ofeach)
+    for i in range(len(ofeach)):
+        g=(ofeach[i]/countattr)**2
+        #g=g*len(data)
+       # print(str(ofeach[i])+"/"+str(countattr)+"="+str(g))
+        finali-=g
+    finali = finali*(countattr/len(data))
+    return (finali,refug,len(abs))
 
 
-# Use DFS to traverse tree and print nicely with appropriate indent
-    # @param node traversing Node
-    # @param indent appropriate indent for each level
 def traverse_tree(node, indent):
     # Print out current node with appropriate indent
     for i in range(indent):
@@ -172,9 +179,49 @@ def traverse_tree(node, indent):
     for i in range(node.get_n_child()):
         traverse_tree(children[i], indent + 1)
         
+def mayday(data):
+    final = []
+    finali=[]
+    sols = set()
+    suls = set()
+    for ele in data:
+        if final.count(ele)>0:
+            finali[finali.index(ele)+1]+=1
+            #print(finali[finali.index(ele)+1])
+        else:
+            suls.add(ele[1])
+            sols.add(ele[0])
+            final.append(ele)
+            finali.append(ele)
+            finali.append(1)
+   # print("\n\n")
+    #print(sols)
+    qm = sols.__iter__()
+    #for ele in finali:
+    #    print(ele)
+    final=[]
+    for j in range(len(sols)):
+        max = []
+        maxn = 0
+        z=qm.next()
+        for i in range(len(finali)/2):
+            #print(str(finali[(i*2)][0])+" of "+str(z))
+            if finali[(i*2)+1]>maxn and finali[(i*2)][0]==z:
+                max=finali[i*2]
+                maxn = finali[(i*2)+1]
+        final.append(max)
+    printee(finali)
+    return final
+    
+	
 
+def write_string(txt,output):
+    f = open(txt, 'w')
+    f.write(str(output))
+    f.close();
+    
 def printee(what):
-	print('\n')
+	#print('\r\n')
 	for ele in what:
 		print(ele)
 
