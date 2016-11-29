7b2cdc01cf30d3cb4406a08623927fb4c2d56e98
Joseph Murphy <murphy53@buffalo.edu>
2016-11-28 22:06:39 -0500

    Add files via upload

--- DTree.py
+++ DTree.py
@@ -1,6 +1,5 @@
 import sys 
 import csv
-from Node import  Node
 solcnt= 0
 global data_file, n_record, n_attr, input_data, visited
 
@@ -19,31 +18,20 @@ def config(args = "golf_processed.csv"):
             vec = [int(i) for i in line]
             input_data.append(vec)
             n_data = len(vec)
-
     n_record = m_data
     n_attr = n_data - 1
     visited = [0] * n_attr
 
 
-# Main function
 def execute():
     master = master_maker(input_data)
     global solcnt
     solcnt = 0
-    #print(master)
     solcnt = len(master[len(master)-1])
-    #print(solcnt)
-    root = Node()
     baka = "root\n"+str(build_tree(input_data,master,"",[]))
-    print("CAKA WAKA SHAKA")
     print(baka)
     write_string('output.txt',baka)
-    #print(input_data)
-    traverse_tree(root, 0)
 
-# Function to build Tree
-    # @param data   Data of passed in Node including Attribute data and label data
-    # @param parent passed in Node
 def attrless(indx,data):
     o1 = data[0:indx]
     o2 = data[indx+1:len(data)]
@@ -66,19 +54,15 @@ def master_maker(input_data):
     return master
     
 def build_tree(data,master,ts,off):
-    #print("Building Tree")
     baka=""
     nts = ""
-    #print(len(ts))
     for i in range(len(ts)):
-        #print(ts[i])
         if ts[i]=='|':
             nts+="|"
         else:
             nts+=" "
     if len(data[0])<3:
         c=mayday(data)
-        print(c)
         for ele in c:
             baka+=nts+"|->(A"+str(len(off))+":"+str(ele[0])+")->"+str(ele[1])+"\n"
         return '!'+baka
@@ -105,8 +89,6 @@ def build_tree(data,master,ts,off):
             newlists=refug[:]
             next = i
             newlen=lens[:]
-    #printee(newlists)
-    #print(off,next)
     ww = 0
     for ele in off:
         if ww>=ele:
@@ -152,32 +134,13 @@ def find_gini(att, lab, data, lst):
             cc+=1.0
     for i in range(len(thistoo)):
         ofeach[thistoo[i]-1]+=1.0
-    #print("HHAHAH"+str(ofeach))
     finali = 1.0
-    #print(ofeach)
     for i in range(len(ofeach)):
         g=(ofeach[i]/countattr)**2
-        #g=g*len(data)
-       # print(str(ofeach[i])+"/"+str(countattr)+"="+str(g))
         finali-=g
     finali = finali*(countattr/len(data))
     return (finali,refug,len(abs))
 
-
-def traverse_tree(node, indent):
-    # Print out current node with appropriate indent
-    for i in range(indent):
-        print("'\t', end=")
-    if (node.get_parent() is None):
-        print("root")
-    else:
-        print("-/", node.get_data())
-
-    # Recursive call all the children nodes
-    children = []
-    children = node.get_children()
-    for i in range(node.get_n_child()):
-        traverse_tree(children[i], indent + 1)
         
 def mayday(data):
     final = []
@@ -187,30 +150,23 @@ def mayday(data):
     for ele in data:
         if final.count(ele)>0:
             finali[finali.index(ele)+1]+=1
-            #print(finali[finali.index(ele)+1])
         else:
             suls.add(ele[1])
             sols.add(ele[0])
             final.append(ele)
             finali.append(ele)
             finali.append(1)
-   # print("\n\n")
-    #print(sols)
     qm = sols.__iter__()
-    #for ele in finali:
-    #    print(ele)
     final=[]
     for j in range(len(sols)):
         max = []
         maxn = 0
         z=qm.next()
         for i in range(len(finali)/2):
-            #print(str(finali[(i*2)][0])+" of "+str(z))
             if finali[(i*2)+1]>maxn and finali[(i*2)][0]==z:
                 max=finali[i*2]
                 maxn = finali[(i*2)+1]
         final.append(max)
-    printee(finali)
     return final
     
 	
@@ -221,7 +177,6 @@ def write_string(txt,output):
     f.close();
     
 def printee(what):
-	#print('\r\n')
 	for ele in what:
 		print(ele)
 
@@ -234,5 +189,3 @@ def main():
     execute()
 
 main()
-
-
